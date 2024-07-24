7ed5937417108a97605bbd597cd5cedb4e56254e
Leyla Ayyubova <102313083+Leylayyubova@users.noreply.github.com>
2024-07-24 15:42:27 +0400

    gitum

---
+++ src/js/table.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function filterTable(filter) {
+        const tables = document.querySelectorAll('table');
+        tables.forEach(table => {
+            const items = table.querySelectorAll('.ts-item');
+            items.forEach(item => {
+                if (filter === 'all' || item.getAttribute('data-tsmeta') === filter) {
+                    item.style.display = '';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+        });
+    }
+
+    const filterButtons = document.querySelectorAll('ul li');
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            filterButtons.forEach(btn => btn.classList.remove('active'));
+            button.classList.add('active');
+            const filter = button.getAttribute('data-tsfilter');
+            filterTable(filter);
+        });
+    });
+
+    filterTable('all');
+});
